b54830d453d101075dae1da3ea98159bceea67a8
Veronica Kemp <veronicaskemp@gmail.com>
2019-12-11 15:02:33 -0500

    Add files via upload

--- chuck-norris-jokes-master/scripts/index.js
+++ chuck-norris-jokes-master/scripts/index.js
@@ -1,6 +1,8 @@
 const jokeServerAddress = "https://api.chucknorris.io/jokes/random";
 const categoryServerAddress = "https://api.chucknorris.io/jokes/categories";
 
+// Contaier for our joke(s) and buttons.
+// We'll assign it later
 let categoryContainer;
 let categoryHeader;
 let currentCategory;
@@ -9,98 +11,12 @@ let jokeButton;
 let multiJokeButton;
 
 function addressForCurrentCategory() {
-    if (curretCategory) {
-        return `${jokeServerAddress}?category=${currentCategory}`
+    if (currentCategory) {
+        return `${jokeServerAddress}?category=${currentCategory}`;
     } else {
         return jokeServerAddress;
     }
-}
-
-function convertToJson(respose) {
-
-}
-
-function clearJokeContainer() {
-
-}
-
-function categoriesToListItems(categoryArray) {
-    return categoryArray.map(cat => {
-        const el = document.createElement('li');
-        el.textContent = cat;
-        return el;
-    })
-}
-
-function setCategory(category) {
-
-
-}
-
-function addCategoryEventClickHandler(categoryListItems) {
-    categoryListItems.forEach(item => {
-        item.addEventListener('click', () => {
-            setCategory(item.textContent);
-    })
-});
-    return categoryListItems;
-}
-
-function renderCategories(categoryListItems) {
-    categoryListItems.forEach(item => {
-        createCategoryContainer.appendChild(item);
-    });
-}
-
-function fetchCategories() {
-    fetch(categoryServerAddress)
-        .then(r => r.json())
-        .then(categoriesToListItem)
-        .then(addCategoryClickHandler)
-        .then(renderCategories)
-}
-
-function fetchJoke() {
-    return fetch(addressForCurrentCategory())
-        .then(r => r.json())
-        .then(extractJoke)
-        // .then(rendersJokeToPage)
-}
-
-function fetchMultipleJokes(howmany=5) {
-    let Count = 0;]
-    const arrayOfUnfinishedPromises= [];
-    while (count < howMany) {
-        const aPromise = fetchJoke();
-        arrayOfUnfinishedPromises.push(aPromise);
-        // fetchJoke();
-        count += 1;
-    }
-    return Promise.all(arrayOfUnfinishedPromises);
-}
-
-function createJokeContainer() {
-
-}
-
-function createJokeButton() {
-
-}
-
-function createMultipleJokeButton() {
-
-}
-
-function createCategoryHeader(cat) {
-    const
-}
-
-function createCategoryContainer() {
-
-}
-
-function rendersJokeToList() {
-
+    
 }
 
 function convertToJson(response) {
@@ -114,60 +30,128 @@ function printJoke(joke) {
 
 function extractJoke(dataObject) {
     // debugger;
-    // return dataObject.value;
-    return "Knock knock. Who's there? Oakley."
+    // console.log(dataObject.value);
+    return dataObject.value;
+    // return "Knock knock. Who's there? Oakley."
 }
 
 function rendersJokeToPage(jokeString) {
     const h1 = document.createElement('h1');
     h1.textContent = jokeString;
-    document.body.appendChild(h1);
+    // document.body.appendChild(h1);
+    jokeContainer.appendChild(h1);
 }
 
+function clearJokeContainer() {
+    jokeContainer.textContent = '';
+}
 
-const whatIsFetch = fetch(jokeServerAddress)
-    // .then(r => r.json())       // Hipster version
-    // .then(convertToJson)          // Named-function version
-    // .then(extractJoke)
-    // .then(rendersJokeToPage)
+function categoriesToListItems(categoryArray) {
+    return categoryArray.map(cat => {
+        const el = document.createElement('li');
+        el.textContent = cat;
+        return el;
+    })
+}
+
+function setCategory(category) {
+    currentCategory = category;
+    updateCategoryHeader(category);
+    console.log(`you clicked ${category}`)
+}
+
+function addCategoryClickHandler(categoryListItems) {
+    categoryListItems.forEach(item => {
+        item.addEventListener('click', () => {
+            setCategory(item.textContent);
+            
+        })
+    });
+
+    return categoryListItems;
+}
+
+function renderCategories(categoryListItems) {
+    categoryListItems.forEach(item => {
+        categoryContainer.appendChild(item);
+    });
+}
+
+function fetchCategories() {
+    fetch(categoryServerAddress)
+        .then(r => r.json())
+        .then(categoriesToListItems)
+        .then(addCategoryClickHandler)
+        .then(renderCategories)
+}
+
+function fetchJoke() {
+    fetch(addressForCurrentCategory())
+        .then(r => r.json())       // Hipster version
+        // .then(convertToJson)          // Named-function version
+        .then(extractJoke)
+        .then(rendersJokeToPage)
+}
+
+function fetchMultipleJokes(howMany=5) {
+    let count = 0;
+    while (count < howMany) {
+        fetchJoke();
+        count += 1;
+    }
+}
+
+// const whatIsFetch = fetch(jokeServerAddress)
+function createJokeContainer() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    return container;
+}
+
+function createJokeButton() {
+    const button = document.createElement('button');
+    button.textContent = 'Tell me a joke!';
+    document.body.appendChild(button);
+    return button;    
+}
+
+function createMultipleJokeButton() {
+    const button = document.createElement('button');
+    button.textContent = 'Tell me A LOT of jokes!';
+    document.body.appendChild(button);
+    return button;    
+}
+
+function updateCategoryHeader(cat) {
+    categoryHeader.textContent = `Current category: ${cat}`;
+}
+
+function createCategoryHeader() {
+    const categoryHeader = document.createElement('h2');
+    document.body.appendChild(categoryHeader);
+    return categoryHeader;    
+}
 
 function createCategoryContainer() {
-
+    const container = document.createElement('ul');
+    document.body.appendChild(container);
+    return container;    
 }
 
 jokeContainer = createJokeContainer();
-categoryContainer = createCategoryHeader();
+categoryContainer = createCategoryContainer();
 categoryHeader = createCategoryHeader();
 
 jokeButton = createJokeButton();
 jokeButton.addEventListener('click', () => {
     clearJokeContainer();
-    fetchJoke()
-        .then(renderJokeToPage)
+    fetchJoke();
 });
-
 
 multiJokeButton = createMultipleJokeButton();
 multiJokeButton.addEventListener('click', () => {
     clearJokeContainer();
-    jokeContainer.textContent = "please wait...";
-    fetchMultipleJokes()
-        .then(joke => {
-            clearJokeContainer();
-            return jokeServerAddress;
-        })
-        .then(renderBatchOfJokes);
-        // .then(showMeTheTHings)
-        // .then(rendersJokeToPage)
+    fetchMultipleJokes();
 });
-
-function renderBatchOfJokes(arrayOfResults) {
-    console.log(arrayOfResults);
-    arrayOfResults
-        .map(rendersJokeToPage)
-//    arrayOfJokePromises
-//         .map(extractJoke)
-//         .map(rendersJokeToPage)
-} 
 
 fetchCategories();
